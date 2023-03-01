0f10679c0828aec7021e882e0d4242b546e81277
Ankit Tomar <ankitjdec@gmail.com>
2023-03-01 06:48:04 +0530

    list important manupulations

---
+++ Python/list.py
@@ -0,0 +1,45 @@
+#Insertion Of Items
+l=[10,20,30,40,50,60,70,80]
+
+# l.append(30)
+# print(l)
+
+# l.insert(1,15)
+# print(l)
+
+# print(15 in l)
+
+# print(l.count(30))
+
+# print(l.index(30))
+
+# print(l.index(30,4,7))
+
+#Removal of Items
+
+# l.remove(20)
+# print(l)
+
+# print(l.pop())
+
+# print(l)
+
+# print(l.pop(2))
+
+# del l[1]
+
+# print(l)
+
+# del l[0:2]
+
+# print(l)
+
+#Some More general purpose :
+
+print(max(l))
+print(min(l))
+print(sum(l))
+l.reverse()
+print(l)
+l.sort()
+print(l)
---
+++ Python/p11.py
@@ -0,0 +1,7 @@
+#Sieve of Erathosthenes
+
+#find all the prime numbrs upto given n
+
+#Naive Approach
+
+
