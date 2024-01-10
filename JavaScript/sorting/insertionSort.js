b8cdaf8e57021ca7f2a8474135d7b4dd50a90bed
Ankit Tomar <ankitjdec@gmail.com>
2024-01-10 20:02:05 +0530

    insertion sort complted

--- JavaScript/sorting/insertionSort.js
+++ JavaScript/sorting/insertionSort.js
@@ -1,9 +1,19 @@
-let array = [12, 11, 13, 6, 5];
+let array = [];
+for (let i = 100; i > 0; i--) {
+  array.push(i);
+}
 function InsertionSort(data) {
   let i, j, current;
   for (i = 1; i < data.length; i++) {
-    console.log(data[i]);
+    current = data[i];
+    j = i - 1;
+    while (j >= 0 && data[j] > current) {
+      data[j + 1] = data[j];
+      j--;
+    }
+    data[j + 1] = current;
   }
 }
 
 InsertionSort(array);
+console.log(array);
