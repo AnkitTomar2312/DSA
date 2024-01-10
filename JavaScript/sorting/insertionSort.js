910d0ce78b63e09cb4e4d37bbbd67aabc589ae43
Ankit Tomar <ankitjdec@gmail.com>
2024-01-10 19:53:00 +0530

    basic check in node

---
+++ JavaScript/sorting/insertionSort.js
@@ -0,0 +1,9 @@
+let array = [12, 11, 13, 6, 5];
+function InsertionSort(data) {
+  let i, j, current;
+  for (i = 1; i < data.length; i++) {
+    console.log(data[i]);
+  }
+}
+
+InsertionSort(array);
