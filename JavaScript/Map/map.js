0263a1b5f7929e60d60b40697258bdb4b0337d8f
Ankit Tomar <ankitjdec@gmail.com>
2024-01-14 17:36:59 +0530

    map data structure complete

--- JavaScript/Map/map.js
+++ JavaScript/Map/map.js
@@ -1,1 +1,25 @@
-//
+//{name=>Ankit}
+//In map we can use any data type
+let data = new Map([
+  ["name", "ankit"],
+  [true, "bool key"],
+  [100, "hundred"],
+]);
+
+data.set("color", "green");
+// console.log(data.size);
+// console.log(data.has(100));
+// console.log(data.get(100));
+// data.clear();
+
+// for (x of data) {
+//   console.log(x);
+// }
+
+// data.forEach((val, key) => {
+//   console.log(key);
+// });
+
+data.delete("name");
+
+console.log(data);
