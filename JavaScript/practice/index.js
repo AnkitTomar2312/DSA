9c55505300d56aaae3302d8ca144d595ac0380ea
Ankit Tomar <ankitjdec@gmail.com>
2024-01-26 19:05:20 +0530

    repeating string prac

--- JavaScript/practice/index.js
+++ JavaScript/practice/index.js
@@ -1,13 +1,10 @@
-let array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-let solution = [];
-let counter = 0;
+let array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 3, 9, 4];
+let key = 0;
 for (let i = 0; i < array.length; i++) {
-  if (array[i] % 2 == 0) {
-    solution.push(array[i] * 2);
-    counter++;
+  key = array[i];
+  for (let j = i + 1; j < array.length; j++) {
+    if (key === array[j]) {
+      console.log(key);
+    }
   }
 }
-
-console.log(
-  "The solution is " + solution + " The length of solution is " + counter
-);
