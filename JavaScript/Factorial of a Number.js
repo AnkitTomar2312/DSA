2a7fb4f84c48db1cd062024c08f8d36fd18bd4d3
Ankit Tomar <ankitjdec@gmail.com>
2024-01-29 18:00:27 +0530

    Factorial of a Number recursive  approach

--- JavaScript/Factorial of a Number.js
+++ JavaScript/Factorial of a Number.js
@@ -1,6 +1,20 @@
-let num = 5;
-let res = 1;
-for (let i = 2; i <= num; i++) {
-  res = res * i;
+//ITERATUVE APPROACH:-
+
+// let num = 5;
+// let res = 1;
+// for (let i = 2; i <= num; i++) {
+//   res = res * i;
+// }
+// console.log(res);
+
+//RECURSIVE APPROACH:-
+
+let num = 10;
+function Fact(num) {
+  if (num == 0) {
+    return 1;
+  }
+  return num * Fact(num - 1);
 }
-console.log(res);
+
+console.log(Fact(num));
