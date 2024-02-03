aed413ad57a65f636736be33f7cc53cd86f8535e
Ankit Tomar <ankitjdec@gmail.com>
2024-02-03 22:28:33 +0530

    SoE complted

--- JavaScript/Sieve of Eratosthenes.js
+++ JavaScript/Sieve of Eratosthenes.js
@@ -1,1 +1,18 @@
+function SoF(n) {
+  if (n <= 1) {
+    return;
+  }
+  let arr = new Array(n + 1).fill(true);
+  let i = 2;
+  while (i <= n) {
+    if (arr[i]) {
+      console.log(i);
+    }
+    for (let j = i * i; j <= n; j += i) {
+      arr[j] = false;
+    }
+    i++;
+  }
+}
 
+SoF(10);
