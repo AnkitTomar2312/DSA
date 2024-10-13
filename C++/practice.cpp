f118325882e2d53e15f1cc7dd58edf21886e7250
Ankit Tomar <ankitjdec@gmail.com>
2024-10-13 11:33:48 +0530

    prac

---
+++ C++/practice.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a=9;
+    if(a==9){
+        cout<<"NINEY";
+    }
+    if(a>0){
+        cout<<"POSITIVE";
+    }
+    else{
+        cout<<"NEGATIVE";
+    }
+    cout<<"\n";
+    int n=24;
+    if(n>20){
+        cout<<"Love";
+    }
+    else if(n==24){
+        cout<<"Lovely";
+    }
+    else{
+        cout<<"Babbar";
+    }
+    cout<<n;
+}
+
