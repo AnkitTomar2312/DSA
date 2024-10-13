653a266b8ba714d8d27917c824f8953b759f1bdd
Ankit Tomar <ankitjdec@gmail.com>
2024-10-13 18:23:25 +0530

    practice code

--- C++/practice.cpp
+++ C++/practice.cpp
@@ -1,27 +1,46 @@
 #include<iostream>
 using namespace std;
 int main(){
-    int a=9;
-    if(a==9){
-        cout<<"NINEY";
-    }
-    if(a>0){
-        cout<<"POSITIVE";
-    }
-    else{
-        cout<<"NEGATIVE";
-    }
-    cout<<"\n";
-    int n=24;
-    if(n>20){
-        cout<<"Love";
-    }
-    else if(n==24){
-        cout<<"Lovely";
-    }
-    else{
-        cout<<"Babbar";
-    }
-    cout<<n;
+    // int a=9;
+    // if(a==9){
+    //     cout<<"NINEY";
+    // }
+    // if(a>0){
+    //     cout<<"POSITIVE";
+    // }
+    // else{
+    //     cout<<"NEGATIVE";
+    // }
+    // cout<<"\n";
+    // int n=24;
+    // if(n>20){
+    //     cout<<"Love";
+    // }
+    // else if(n==24){
+    //     cout<<"Lovely";
+    // }
+    // else{
+    //     cout<<"Babbar";
+    // }
+    // cout<<n;
+int i=10,j=1;
+while(j<i){
+char c;
+cout<<"Enter a number"<<endl;
+cin>>c;
+int  num=int(c);
+if(num>96 && num<123){
+    cout<<c<<" is a Lower Alphabet";
+}
+else if(num>64 && num<91){
+    cout<<c<<" is a Capital Alphabet";
+}
+else if(num>47 && num<58){
+ cout<<c<<" is a Number";   
+}
+cout<<endl;
+j++;
+}
+return 0;
 }
 
