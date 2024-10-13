1780952e6388ef2784d7b4bec841c05927ab1ac4
Ankit Tomar <ankitjdec@gmail.com>
2024-10-13 12:20:26 +0530

    number box patter

--- C++/pattern.cpp
+++ C++/pattern.cpp
@@ -1,15 +1,24 @@
 #include<iostream>
 using namespace std;
 int main(){
-    int rows,cols,i=1;
+    int rows,cols,i=0;
     cout<<"Enter the number of rows"<<endl;
     cin>>rows;
     cout<<"Enter the number of cols"<<endl;
     cin>> cols;
-    while(i<=rows){
-        int j=1;
-        while(j<=cols){
-            cout<<"*"<<" ";
+    // while(i<=rows){
+    //     int j=1;
+    //     while(j<=cols){
+    //         cout<<"*"<<" ";
+    //         j++;
+    //     }
+    //     cout<<endl;
+    //     i++;
+    // }
+    while(i<rows){
+        int j=0;
+        while(j<cols){
+            cout<<i<<"";
             j++;
         }
         cout<<endl;
