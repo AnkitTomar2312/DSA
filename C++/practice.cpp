2ee8374876caa5baf925b6ac3e7865c006de4bf8
Ankit Tomar <ankitjdec@gmail.com>
2024-10-15 19:05:02 +0530

    fib prac

--- C++/practice.cpp
+++ C++/practice.cpp
@@ -23,23 +23,31 @@ int main(){
     //     cout<<"Babbar";
     // }
     // cout<<n;
-int i=10,j=1;
-while(j<i){
-char c;
-cout<<"Enter a number"<<endl;
-cin>>c;
-int  num=int(c);
-if(num>96 && num<123){
-    cout<<c<<" is a Lower Alphabet";
-}
-else if(num>64 && num<91){
-    cout<<c<<" is a Capital Alphabet";
-}
-else if(num>47 && num<58){
- cout<<c<<" is a Number";   
-}
-cout<<endl;
-j++;
+// int i=10,j=1;
+// while(j<i){
+// char c;
+// cout<<"Enter a number"<<endl;
+// cin>>c;
+// int  num=int(c);
+// if(num>96 && num<123){
+//     cout<<c<<" is a Lower Alphabet";
+// }
+// else if(num>64 && num<91){
+//     cout<<c<<" is a Capital Alphabet";
+// }
+// else if(num>47 && num<58){
+//  cout<<c<<" is a Number";   
+// }
+// cout<<endl;
+// j++;
+// }
+int n=10,a=0,b=1;
+cout<<a<<" "<<b<<" ";
+for(int i=0;i<=n;i++){
+    int nextN=a+b;
+    cout<<nextN<<" ";
+    a=b;
+    b=nextN;
 }
 return 0;
 }
