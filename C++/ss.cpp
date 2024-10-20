c49ef747ff3365b014debb02aa48749f4992e092
Ankit Tomar <ankitjdec@gmail.com>
2024-10-20 17:49:04 +0530

    o/p-2

--- C++/ss.cpp
+++ C++/ss.cpp
@@ -1,13 +1,22 @@
 #include<iostream>
 using namespace std;
+// void update(int a){
+//     a=a/2;
 
-void update(int a){
-    a=a/2;
+// }
 
+// int main(){
+//     int a=10;
+// update(a);
+// cout<<a<<endl;
+// }
+int update(int a){
+
+    a-=5;
+    return a;
 }
-
 int main(){
-    int a=10;
-update(a);
-cout<<a<<endl;
+    int a=15;
+    update(a);
+    cout<<a<<endl;
 }
