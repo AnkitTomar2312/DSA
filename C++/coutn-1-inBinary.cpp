0ccbd4c6c69a771c3fd885baa97a07767485b887
Ankit Tomar <ankitjdec@gmail.com>
2024-10-20 19:11:31 +0530

    count-1-binary

---
+++ C++/coutn-1-inBinary.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+using namespace std;
+
+int counter(int a)
+{
+    int counter = 0;
+    while (a != 0)
+    {
+        int dig = a % 10;
+        if (dig == 1)
+        {
+            counter++;
+        }
+        a/=10;
+    }
+    cout<<"counter loop "<< counter<<endl;
+    
+}
+
+int decToBin(int n)
+{
+    string toBin = "";
+    while (n != 0)
+    {
+        int bit = n & 1;
+        toBin = to_string(bit) + toBin;
+        n = n >> 1;
+    }
+    int binary=stoi(toBin);
+    cout<<"decToBin Loop"<<binary<<endl;
+    counter(binary);
+
+}
+
+
+
+int main()
+{
+    int a, b;
+    cin >> a>>b;
+    decToBin(a);
+    decToBin(b);
+    
+}
